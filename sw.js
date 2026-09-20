e35a185541520d59fe62cdb7ba138b3d38c00668
juniorrenosto <junior.renosto@gmail.com>
2026-09-19 21:22:59 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service Worker para Inclusão Conectada (PWA / TWA)
+const CACHE_NAME = 'inclusao-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Ignora requisições de API para não travar respostas dinâmicas
+  if (e.request.url.includes('script.google.com')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
